33e4fc3609420e42bf08df63e504b6d8b75b3df5
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-02-10 15:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+const strong=document.querySelectorAll('strong');
 function highlight() {
     //Write your code here
-
+strong.forEach(element=>{
+	element.style.color="rgb(0, 128, 0)";
+})
 
 }
-
-
 function return_normal() {
     //Write your code here
-
+strong .forEach(element=>{
+	element.style.color="rgb(0, 0, 0)";
+})
     
 }
